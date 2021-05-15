0369fefbb443b1428fadea6cbce1fb42c39877f7
Arthur <77768569+Archi2903@users.noreply.github.com>
2021-05-15 12:10:04 +0300

    Add files via upload

---
+++ template/article1.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/edit.ico" type="image/x-icon">
+    <!--    Link Bootstrap4.4-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+    <!--    Google Fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Noto+Sans+HK&display=swap" rel="stylesheet">
+    <!--    Link Css-->
+    <link rel="stylesheet" href="style.css">
+    <title>Article1</title>
+</head>
+<body class="container">
+<div class="row">
+<p class="col-sm-11">
+
+    <p class="display-4"> Bitcoin fell by more than 10% after the tweet, while Tesla shares also dipped.</p>
+
+    Tesla's announcement in March that it would accept the cryptocurrency was met
+    with an outcry from some
+    environmentalists and investors.
+
+    The electric carmaker had in February revealed it had bought $1.5bn (£1bn) of
+    the world's biggest digital currency.
+
+    Elon Musk's SpaceX to launch dogecoin moon mission
+    Bitcoin could derail China's climate change targets
+    How Bitcoin's vast energy use could burst its bubble
+    But on Thursday, it backtracked on its previous comments.
+
+    "We are concerned about rapidly increasing use of fossil fuels for Bitcoin
+    mining and transactions,
+    especially coal, which has the worst emissions of any fuel," Mr Musk wrote.
+
+    "Cryptocurrency is a good idea... but this cannot come at great cost to the
+    environment."
+
+    He also said the electric carmaker would not sell any of its Bitcoin,
+    and intends to use
+    it for transactions as soon as mining shifts to using more
+    sustainable energy.</p>
+</div>
+<img src="/img/Screen1.png" class="img-thumbnail">
+<div class="row">
+<p class="col-sm-10">Market analysts see the move as an attempt by Tesla to assuage
+    the concerns of investors who are focused
+    on climate change and sustainability.
+
+    "Environmental, Social and Corporate Governance (ESG) issues are now
+    a major motivation for many investors.
+    Tesla, being a clean energy-focused company, might want to work better
+    in the environmental area of ESG,
+    " Julia Lee from Burman Invest told the BBC.
+
+    "But a cynic might suggest that this is just another move by Elon Musk
+    to influence the cryptocurrency market,
+    as he has done on so many other occasions," she added.</p>
+</div>
+</body>
+</html>
---
+++ template/article2.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/edit.ico" type="image/x-icon">
+    <!--    Link Bootstrap4.4-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+    <!--    Google Fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Noto+Sans+HK&display=swap" rel="stylesheet">
+    <!--    Link Css-->
+    <link rel="stylesheet" href="style.css">
+    <title>Article1</title>
+</head>
+<body class="container">
+<div class="row">
+
+    <p class="col-sm-11">
+
+    <p class="display-4"> The European Commission had ordered the tech giant to repay the funds as back taxes, alleging
+        that Amazon had been
+        given unfair special treatment.</p>
+
+    But the EU's General Court overturned that order, finding it had been given "no selective advantage".
+
+    Amazon said the decision was "in line with our long-standing position that we followed all applicable laws".
+
+    "We're pleased that the court has made this clear, and we can continue to focus on delivering for our customers
+    across Europe," the company said in a statement.
+
+    The contentious order dates back to 2017, when the European Competition Commissioner, Margrethe Vestager, had turned
+    her attention to tech giants and their tax arrangements in some EU countries.
+
+    The European Commission had alleged that the tax deal amounted to state aid for the company - something which breaks
+    the rules of the EU's internal market.
+
+    Both Amazon and the country of Luxembourg went to court to have the decision overturned.
+
+    "None of the findings set out by the Commission in the contested decision are sufficient to demonstrate the
+    existence
+    of an advantage," the court said in a statement.
+
+    "The contested decision must be annulled in its entirety."
+
+    A much larger tax bill for a similar case with Apple in Ireland - involving €13bn (£11.6bn) - was also overturned
+    last
+    year. The Commission lodged an appeal in September.
+
+    Europe to fight Apple 13bn euro tax bill decision
+    Amazon charged with abusing EU competition rules
+    It is not yet clear if the EU will appeal against the latest Amazon ruling.
+
+    In a statement, Margrethe Vestager said: "All companies should pay their fair share of tax."</p>
+</div>
+<img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9/production/_118477740_gettyimages-857395328.jpg"
+     class="img-thumbnail">
+<p class="col-sm-11">She said the Amazon-Luxembourg deal meant "three-quarters of the profits made from all Amazon sales
+    in the
+    EU went untaxed until 2014".
+
+    "We will carefully study the judgment and reflect on possible next steps."
+
+    The court also ruled separately on French energy firm Engie, which had been ordered to pay €120m (£103m) back to
+    Luxembourg. The court upheld that decision.
+
+    Despite the setbacks, the Commission's pressure on tech giants to pay more tax has indirectly led to changes in tax
+    systems used by some EU members.
+
+    An international effort involving the Organisation for Economic Cooperation and Development (OECD) is also aiming to
+    establish a set of common international rules on how multinational corporations should be taxed.
+
+    That could apply to the major tech giants, and has led to friction between some European countries and the
+    United States, where many of the companies are based.
+
+    In January, both France and the UK decided to delay the introduction of a planned "tech tax" while the OECD
+    negotiations continue.
+
+</p>
+</body>
+</html>
